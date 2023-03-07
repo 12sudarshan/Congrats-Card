88a15c07b14da5976671496b61782511ff844288
sudarshangit config --global user.name sudarshan <sudarshanr924@gmail.com>
2023-03-07 08:58:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="profile-name"> sudarshan S </h1>
+      <p className="institute-name">
+        {" "}
+        Vishnu institute of computer education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
